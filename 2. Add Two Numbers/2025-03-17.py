fd3b095f1598fbf9d471c8714de7b36238abf8ac
parker1019 <parker178912.cs12@nycu.edu.tw>
2025-03-17 18:24:47 +0800

    feat(2): Add Two Numbers

---
+++ 2. Add Two Numbers/2025-03-17.py
@@ -0,0 +1,33 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        current = dummy
+        carry = 0
+
+        while carry or l1 or l2:
+            if l1:
+                val1 = l1.val
+            else:
+                val1 = 0
+            if l2:
+                val2 = l2.val
+            else:
+                val2 = 0
+
+            total = val1 + val2 + carry
+            carry = total // 10
+            current.next = ListNode(total % 10)
+            current = current.next
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+        return dummy.next
